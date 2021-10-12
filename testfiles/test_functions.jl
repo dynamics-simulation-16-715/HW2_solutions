d31edda51fa2a6a688b81a2a155cd5dd53dda9ca
Kevin Tracy <ktracy@stanford.edu>
2021-10-12 18:21:06 -0400

    q1 solutions

---
+++ testfiles/test_functions.jl
@@ -0,0 +1,8 @@
+using Test
+function test_vec(var,key,td)
+    for i = 1:length(td[key])
+        name = "element #"*string(i)*" of "*key;
+        @testset "$name" begin @test isapprox(var[i], td[key][i],atol = 1e-10); end;
+    end
+    return nothing 
+end
